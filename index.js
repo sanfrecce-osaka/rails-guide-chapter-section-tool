b76bf9c263bb8a11810806c33704e75dbaa1737e
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2021-08-30 21:25:02 +0900

    refactoring
    - reduced the number of loops

--- index.js
+++ index.js
@@ -8,32 +8,24 @@ const [, , url] = process.argv
   await page.goto(url)
 
   await page.waitForSelector("ol.chapters")
-  const chapters = await page.$$eval("ol.chapters > li > a", (targets) => targets.map((target) => target.innerText))
+  const chapters = await page.$$("ol.chapters > li")
 
-  const lists = await page.$$("ol.chapters > li")
-
-  const sections = await Promise.all(lists.map(async (list) => await list.$("ul")))
-  const results = await Promise.all(
-    chapters.map(async (chapter, i) => {
-      if (!sections[i]) return { chapter, sections: [] }
-      const sectionTitles = await sections[i].$$eval("li > a", (sectionList) =>
-        sectionList.map((body) => body.innerText)
-      )
-      return { chapter, sections: sectionTitles }
-    })
+  const titles = await Promise.all(
+    chapters.map(async (chapter) => await chapter.$$eval("a", (anchors) => anchors.map(({ innerText }) => innerText)))
   )
-  results.forEach((result, chapterIndex) => {
-    console.log(`### ${chapterIndex + 1} ${result.chapter}`)
+
+  titles.forEach(([chapterTitle, ...sectionTitles], chapterIndex) => {
+    console.log(`### ${chapterIndex + 1} ${chapterTitle}`)
     console.log()
 
-    if (result.sections.length <= 0) {
+    if (sectionTitles.length <= 0) {
       console.log("- ")
       console.log()
       return
     }
 
-    result.sections.forEach((section, sectionIndex) => {
-      console.log(`#### ${chapterIndex + 1}.${sectionIndex + 1} ${section}`)
+    sectionTitles.forEach((sectionTitle, sectionIndex) => {
+      console.log(`#### ${chapterIndex + 1}.${sectionIndex + 1} ${sectionTitle}`)
       console.log()
       console.log("- ")
       console.log()
